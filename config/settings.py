a4b5aedf95d3bdb2b80c1c54aa4771a58bd70284
selfsryo <selfsryo@gmail.com>
2021-02-12 00:47:02 +0900

    ページネーション更新

--- config/settings.py
+++ config/settings.py
@@ -150,11 +150,6 @@ if DEBUG:
         'http://localhost:3000',
     )
 
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2
-}
-
 
 if DEBUG:
     INSTALLED_APPS += ['corsheaders']
